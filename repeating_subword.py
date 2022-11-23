243799e8f85e9a23159b8288370e647c09f9279c
Kevin Csiffary <kevin.csiffary@gmail.com>
2022-11-23 17:26:22 +0000

    Created repeating_subword.py

---
+++ repeating_subword.py
@@ -0,0 +1,67 @@
+# !/user/bin/env python3
+
+# Created by Kevin Csiffary
+# Date: Nov. 14, 2022
+# This program prints "Hello, World!" and has comments
+
+
+def inputs():
+    # makes the variables global so they can be accessed later in the code
+    global user_string
+    global num_repeat_str
+    global subword_len_str
+
+    # gets all required user inputs
+    user_string = input("Please enter a string: ")
+    num_repeat_str = input("Please enter the number of copies: ")
+    subword_len_str = input("Please enter the length of the subword: ")
+
+
+# error checking
+def errors():
+    # access global variables
+    global num_repeat_int
+    global subword_len_int
+
+    try:
+        # cast the user inputs into integers
+        num_repeat_int = int(num_repeat_str)
+        subword_len_int = int(subword_len_str)
+
+        # checks if the inputs are less than or equal to zero
+        if (num_repeat_int <= 0) | (subword_len_int <= 0):
+            print("Please enter a positive integer")
+
+    # catch errors
+    except:
+        print("Please enter a valid integer")
+        exit()
+
+
+def get_subword():
+    # access global variables
+    global subword
+
+    # sets the subword to the cut initial word
+    subword = str(user_string[:subword_len_int])
+
+
+def loop():
+    # loop through and repeat the sub word the user specified amount of times
+    for counter in range(num_repeat_int):
+        print(subword, end="")
+
+    # once the loop has completed add extra line
+    print()
+
+
+# calls all of the functions in the correct order
+def main():
+    inputs()
+    errors()
+    get_subword()
+    loop()
+
+
+if __name__ == "__main__":
+    main()
